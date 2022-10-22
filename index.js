731514274492970c825c52f9bd5307a32f8cfc38
ilona <gogiashvili.ilona@gmail.com>
2022-10-22 15:28:33 -0400

    commit tree d40e2b98dc2801c6f88decb558632e63e57adc29

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
 // Write your solution in this file!
+
+
+var customerName = "bob";
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+
+
+//function setBestCustomer
+function setBestCustomer() {
+    return bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    return bestCustomer = 'maybe bob';
+}
+
+const leastFavoriteCustomer = 'brian';
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'christian';
+}
+
+
+
+
+
+
+
+/*
+var customerName = 'bob';
+function upperCaseCustomerName() {
+    return customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = "not bob";
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = "maybe bob";
+
+}
+
+
+const leastFavoriteCustomer = "david";
+
+
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = "david"
+}
+*/
